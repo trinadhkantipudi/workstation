6f98461996e697a039e41ce8e636376e33c42609
trinadhkantipudi <56853496+trinadhkantipudi@users.noreply.github.com>
2020-06-04 21:43:08 -0700

    Update ec2.tf

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -1,27 +1,21 @@
 provider "aws" {
   region = "us-east-2"
 }
-
 data "aws_ami" "CentOs" {
   most_recent = true
-
   filter {
     name   = "name"
     values = ["CentOS Linux 7 x86_64 HVM EBS ENA 2002_01-b7ee8a69-ee97-4a49-9e68-afaee216db2e-ami-0042af67f8e4dcc20.4"]
   }
-
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
   owners = ["679593333241"] # Canonical
 }
-
 resource "aws_instance" "practice" {
   ami           = data.aws_ami.CentOs.image_id
   instance_type = "t2.micro"
-
   tags = {
     Name = "practice"
   }
