6c7d18a1e3fc405414db7f217dc55fe65d9aa99c
trinadhkantipudi <56853496+trinadhkantipudi@users.noreply.github.com>
2020-06-05 18:36:53 -0700

    Update ec2.tf

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "practice" {
       "sudo yum install ansible -y",
       "sudo yum install git -y",
       "echo localhost>/tmp/hosts",
-      "ansible-pull -i /tmp/hosts -U https://github.com/naveenthangella/Practice/blob/master/Ansible/playbooks/main.yml"
+      "ansible-pull -i /tmp/hosts -U https://github.com/naveenthangella/Practice.git Ansible/playbooks/main.yml"
 
     ]
   }
